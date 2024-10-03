d8871fb5555c8c27b60e88291665693ff160bda2
plan-out <45305408+plan-out@users.noreply.github.com>
2024-10-03 19:44:43 +0900

    Update script.js

--- vegan eat/js/script.js
+++ vegan eat/js/script.js
@@ -1,7 +1,12 @@
+//문서가 화면에 로드된 후 실행
+window.addEventListener("load",function(){
+    //메인뷰 제품슬라이드 영역
+    const main_image = new Swiper('#main_image', {
+        // Optional parameters
+        loop: true,
+    });
 
-//메인뷰 제품슬라이드 영역
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-});
+
+
+});//end:window.addEventListener(...
+    

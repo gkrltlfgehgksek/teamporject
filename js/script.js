d7cb64d4b195f4e6b276c62ddf9db4a82d40610d
최다흰 <chlekgmls725@naver.com>
2024-10-04 16:25:19 +0900

    10.4일 업로드(최다흰)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//문서가 화면에 로드된 후 실행
+window.addEventListener("load",function(){
+    //메인뷰 제품슬라이드 영역
+    const main_image = new Swiper('#main_image', {
+        // Optional parameters
+        loop: true,
+    });
+
+
+
+
+});//end:window.addEventListener(...
+
+
+    

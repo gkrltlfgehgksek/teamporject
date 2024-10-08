64eb67bb649ea17467a0c3c104b1b6f3e9df13a0
최다흰 <chlekgmls725@naver.com>
2024-10-08 12:43:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//문서가 화면에 로드된 후 실행
+window.addEventListener("load",function(){
+    //메인뷰 제품슬라이드 영역
+    const main_image = new Swiper('#main_image', {
+        // Optional parameters
+        loop: true,
+        effect:'fade',
+        speed:200,
+        autoplay: {
+            delay: 5000,
+        },
+    });
+
+    const product_slider = new Swiper('#product_slider', {
+        // Optional parameters
+        loop: true,
+        pagination: {
+            el: '.pager',
+            type: "fraction",
+        },
+        autoplay: {
+            delay: 5000,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        
+    });
+
+
+});//end:window.addEventListener(...
+
+
+    

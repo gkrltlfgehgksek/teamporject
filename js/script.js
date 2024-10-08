3cb25769fcc4e6b62046166a10b2b683541f364a
plan-out <45305408+plan-out@users.noreply.github.com>
2024-10-08 14:28:46 +0900

    라인 애니메이션 추가

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ window.addEventListener("load",function(){
     const main_image = new Swiper('#main_image', {
         // Optional parameters
         loop: true,
+        effect:'fade',
+        speed:200,
+        autoplay: {
+            delay: 5000,
+        },
     });
 
     const product_slider = new Swiper('#product_slider', {
@@ -24,7 +29,50 @@ window.addEventListener("load",function(){
     });
 
 
+    var content = document.querySelector('.bg_line_box');
+    var path = document.querySelector('.bg_line path');
+    var pathLength = path.getTotalLength();
+    path.style.strokeDasharray  = pathLength;
+    var absoluteTop = window.scrollY + content.getBoundingClientRect().top;
+    var content_h = content.offsetHeight;
+    // path.style.strokeDashoffset = calcDashoffset(window.innerHeight * 0.4, content1, path1Length)
+  
+
+    // function calcDashoffset(scrollY, element, length) {
+    //   const ratio = (scrollY - element.offsetTop) / element.offsetHeight
+    //   const value = length - (length * ratio)
+    //   return value < 0 ? 0 : value > length ? length : value
+    // }
+
+    function scrollHandler() {
+      var point_Y = window.scrollY - absoluteTop + 500;
+      var ratio = point_Y / content_h;
+      var ratio_re = 0
+      if(ratio<0) ratio_re = 0;
+      else if(ratio>1) ratio_re =1;
+      else ratio_re = ratio;
+      var value = pathLength * ratio_re;
+      path.style.strokeDashoffset = -1*(pathLength - value);
+     
+    }
+
+
+    window.addEventListener('scroll', scrollHandler);
+
+
+    	
+	SmoothScroll({
+		frameRate: 120,
+		animationTime: 500,
+		stepSize: 80,
+		pulseAlgorithm: 1,
+		pulseScale: 2,
+		pulseNormalize: 1,
+		accelerationDelta: 25,
+		accelerationMax: 1,
+		keyboardSupport: 1,
+		arrowScroll: 50,
+		fixedBackground: 0
+	});
+   
 });//end:window.addEventListener(...
-
-
-    
